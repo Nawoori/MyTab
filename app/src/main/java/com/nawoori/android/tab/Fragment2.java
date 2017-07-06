e146bdbe071eac1e61ebcfe14974e1270d149549
good edu <goodeducoop@gmail.com>
2017-07-07 03:39:52 +0900

    액션바연습

---
+++ app/src/main/java/com/nawoori/android/tab/Fragment2.java
@@ -0,0 +1,23 @@
+package com.nawoori.android.tab;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by User on 2017-07-07.
+ */
+
+public class Fragment2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment3, container, false);
+
+        return rootView;
+    }
+}
